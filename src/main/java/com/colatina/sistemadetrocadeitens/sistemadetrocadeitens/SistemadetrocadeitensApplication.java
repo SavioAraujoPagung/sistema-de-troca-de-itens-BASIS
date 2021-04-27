09937c5dd346d031f1490daeabcd21be058678a2
SavioAraujoPagung <savio.pagung@hotmail.com>
2021-04-27 18:36:59 -0300

    REPOSITORIO - criei o backend

---
+++ src/main/java/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/SistemadetrocadeitensApplication.java
@@ -0,0 +1,13 @@
+package com.colatina.sistemadetrocadeitens.sistemadetrocadeitens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadetrocadeitensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadetrocadeitensApplication.class, args);
+	}
+
+}
