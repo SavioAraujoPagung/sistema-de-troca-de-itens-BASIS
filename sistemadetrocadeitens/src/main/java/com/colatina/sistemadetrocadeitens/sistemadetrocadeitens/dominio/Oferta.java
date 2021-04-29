068e60632ebd6f7a9e4febc5c3d9525ae93a0c6d
SavioAraujoPagung <savio.pagung@hotmail.com>
2021-04-29 19:56:15 -0300

    BANCO DE DADOS - Criacao da dos changelogs ofertas e itensofertados

---
+++ sistemadetrocadeitens/src/main/java/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/dominio/Oferta.java
@@ -0,0 +1,46 @@
+package com.colatina.sistemadetrocadeitens.sistemadetrocadeitens.dominio;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_oferta")
+public class Oferta {
+	@Id
+    @Column(name = "id")
+    @SequenceGenerator(allocationSize = 1, sequenceName = "seq_tb_item", name = "seq_tb_item")
+    @GeneratedValue(generator = "seq_tb_item", strategy = GenerationType.SEQUENCE)
+    private float i;
+
+	@ManyToMany
+	@JoinTable(name="tb_item_ofertado", joinColumns=
+			{@JoinColumn(name="id_oferta")}, inverseJoinColumns=
+			{@JoinColumn(name="id_item_ofertado")})
+	private List<Item> itensOfertados;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private SituacaoOferta situacao;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_usuario_ofertante")
+	private Usuario usuarioOfertante;
+	
+	@ManyToOne
+    private Item item;    
+}
---
+++ sistemadetrocadeitens/src/main/java/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/dominio/Usuario.java
@@ -0,0 +1,40 @@
+package com.colatina.sistemadetrocadeitens.sistemadetrocadeitens.dominio;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+    @Id
+    @Column(name = "id")
+    @SequenceGenerator(allocationSize = 1, sequenceName = "seq_tb_usuario", name = "seq_tb_usuario")
+    @GeneratedValue(generator = "seq_tb_usuario", strategy = GenerationType.SEQUENCE)
+    private float id;
+    
+    @Column(name = "nome", nullable = false)
+    private String nome;
+    
+    @Column(name = "cpf", nullable = false)
+    private String cpf;
+    
+    @Column(name = "email", nullable = false)
+    private String email;
+    
+    @Column(name = "data", nullable = false)
+    private LocalDate data;
+	
+	@Column(name = "token", nullable = false)
+    private String token;
+}
