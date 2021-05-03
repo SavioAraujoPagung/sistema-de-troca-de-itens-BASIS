b48f608dedad6fd8a4ce9e8f884bfa35122ee9a7
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2021-05-03 14:04:27 -0300

    CRUD ITEM - Conscertei um erro depois de alterar alguma coisa do pom.xml. Nao sei o que foi

--- sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/ItemMapperImpl.java
+++ sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/ItemMapperImpl.java
@@ -3,15 +3,14 @@ package com.colatina.sistemadetrocadeitens.sistemadetrocadeitens.servico.mapper;
 import com.colatina.sistemadetrocadeitens.sistemadetrocadeitens.dominio.Item;
 import com.colatina.sistemadetrocadeitens.sistemadetrocadeitens.servico.dto.ItemDto;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-01T19:14:22-0300",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_292 (Private Build)"
+    date = "2021-05-03T14:00:47-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.11 (Ubuntu)"
 )
 @Component
 public class ItemMapperImpl implements ItemMapper {
@@ -24,16 +23,6 @@ public class ItemMapperImpl implements ItemMapper {
 
         Item item = new Item();
 
-        item.setId( dto.getId() );
-        item.setNome( dto.getNome() );
-        Byte[] imagem = dto.getImagem();
-        if ( imagem != null ) {
-            item.setImagem( Arrays.copyOf( imagem, imagem.length ) );
-        }
-        item.setDescricao( dto.getDescricao() );
-        item.setCategoria( dto.getCategoria() );
-        item.setUsuario( dto.getUsuario() );
-
         return item;
     }
 
@@ -44,16 +33,6 @@ public class ItemMapperImpl implements ItemMapper {
         }
 
         ItemDto itemDto = new ItemDto();
-
-        itemDto.setId( entity.getId() );
-        itemDto.setNome( entity.getNome() );
-        Byte[] imagem = entity.getImagem();
-        if ( imagem != null ) {
-            itemDto.setImagem( Arrays.copyOf( imagem, imagem.length ) );
-        }
-        itemDto.setDescricao( entity.getDescricao() );
-        itemDto.setCategoria( entity.getCategoria() );
-        itemDto.setUsuario( entity.getUsuario() );
 
         return itemDto;
     }
