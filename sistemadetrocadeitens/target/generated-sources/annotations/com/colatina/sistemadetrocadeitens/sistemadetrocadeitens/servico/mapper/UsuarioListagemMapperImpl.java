0bdfce2d73a23a53c18525771c7e754f92fd2922
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2021-05-04 10:18:02 -0300

    REGISTROS DAS TABELAS FIXAS - Varias alteracoes menores, praticamente todas automaticas

--- sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/UsuarioListagemMapperImpl.java
+++ sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/UsuarioListagemMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-04T09:57:24-0300",
+    date = "2021-05-04T10:02:50-0300",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.11 (Ubuntu)"
 )
 @Component
