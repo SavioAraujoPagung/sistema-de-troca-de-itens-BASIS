6d762e941a94260b27181a2fdf033b1b311dcc6a
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2021-05-05 20:46:04 -0300

    TESTE USUARIO - Implementei o UsuarioBuilder

--- sistemadetrocadeitens/src/test/java/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/recurso/UsuarioRecursoIT.java
+++ sistemadetrocadeitens/src/test/java/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/recurso/UsuarioRecursoIT.java
@@ -1,4 +1,11 @@
 package com.colatina.sistemadetrocadeitens.sistemadetrocadeitens.recurso;
 
-public class UsuarioRecursoIT {
+import com.colatina.sistemadetrocadeitens.sistemadetrocadeitens.util.IntTestComum;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@Transactional
+public class UsuarioRecursoIT extends IntTestComum {
 }
