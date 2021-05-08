d76df2487a0cddb18ab56e2febd5cb11570b04d7
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2021-05-08 18:26:08 -0300

    RN 3 - Iniciando a regra de negocio 3

--- sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/UsuarioMapperImpl.java
+++ sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/UsuarioMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-07T19:55:46-0300",
+    date = "2021-05-08T18:23:49-0300",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 1.8.0_292 (Private Build)"
 )
 @Component
