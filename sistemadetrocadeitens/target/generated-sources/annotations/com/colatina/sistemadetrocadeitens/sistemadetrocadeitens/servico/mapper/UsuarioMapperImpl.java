114c2352dba2b387ffbc78bf933c1d804e3d6013
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2021-05-10 02:00:22 -0300

    mend

--- sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/UsuarioMapperImpl.java
+++ sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/UsuarioMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-10T01:54:48-0300",
+    date = "2021-05-10T01:59:01-0300",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.11 (Ubuntu)"
 )
 @Component
