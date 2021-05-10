c81b1662aba20fe50b57b2ede3cb93b84b5db67e
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2021-05-09 21:00:51 -0300

    mend

--- sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/ItemMapperImpl.java
+++ sistemadetrocadeitens/target/generated-sources/annotations/com/colatina/sistemadetrocadeitens/sistemadetrocadeitens/servico/mapper/ItemMapperImpl.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-09T19:52:40-0300",
-    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 1.8.0_292 (Private Build)"
+    date = "2021-05-09T20:57:59-0300",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.11 (Ubuntu)"
 )
 @Component
 public class ItemMapperImpl implements ItemMapper {
