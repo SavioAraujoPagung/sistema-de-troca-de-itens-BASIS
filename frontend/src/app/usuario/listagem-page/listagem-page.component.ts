58ea42287de770ce2e1976b8a116357786d514af
SavioAraujoPagung <savio.pagung@hotmail.com>
2021-05-13 21:31:11 -0300

    CRUD USUARIO - funcionando

--- frontend/src/app/usuario/listagem-page/listagem-page.component.ts
+++ frontend/src/app/usuario/listagem-page/listagem-page.component.ts
@@ -1,6 +1,9 @@
-import { UsuarioService } from './../../services/usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PageNotificationService } from '@nuvem/primeng-components';
+import { finalize } from 'rxjs/operators';
+
+import { UsuarioService } from './../../services/usuario.service';
 
 @Component({
   selector: 'app-listagem-page',
@@ -9,44 +12,108 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ListagemPageComponent implements OnInit {
 
-  usuarios: any[] = []
-
-  displayModal: boolean = false
-
+  usuarios: any[] = [];
+  displayModal: boolean = false;
+  submit: boolean = false;
   form: FormGroup;
+  isEditing: boolean = false;
 
-  constructor(private usuarioService: UsuarioService, private fb: FormBuilder) {
+  constructor(
+    private usuarioService: UsuarioService,
+    private fb: FormBuilder,
+    private notification: PageNotificationService) {
 
+  }
+
+  ngOnInit(): void {
+    this.iniciarForm();
+    this.buscarTodo();
   }
 
   buscarTodo(){
     this.usuarioService.buscarTodos().subscribe(
       (usuarios) => {
-        this.usuarios = usuarios
+        this.usuarios = usuarios;
       }
     )
   }
 
   abrirModal(){
-    this.displayModal=true
+    this.displayModal=true;
+  }
+
+  editar(id){
+    this.isEditing=true;
+    this.usuarioService.buscarPorId(id).subscribe(
+      (usuario)=>{
+        this.displayModal=true;
+        this.form.patchValue({
+          ...usuario,
+          data: new Date(usuario.data)
+        }); 
+      })
   }
 
   salvar(){
-    this.usuarioService.salvar(this.form.value);
+    this.submit = true
+    if (this.isEditing){
+      this.usuarioService.atualizar(this.form.value).pipe(
+        finalize(()=>{
+          this.submit = false;
+          this.fecharModal();
+        })
+      ).subscribe(
+        () => {
+          this.buscarTodo();
+          this.notification.addSuccessMessage("Usuario atualizado com sucesso");
+        },
+        ()=>{
+          this.notification.addErrorMessage("Erro ao atualizar usuario");
+        }
+      );
+    }else{
+      this.usuarioService.salvar(this.form.value).pipe(
+        finalize(()=>{
+          this.fecharModal();
+          this.submit = false;
+        })
+      ).subscribe(
+        (usuario) => {
+          this.buscarTodo();
+          this.notification.addSuccessMessage("Usuario Cadastrado com sucesso");
+        },
+        ()=>{
+          this.notification.addErrorMessage("Erro ao cadastrar usuario");
+        }
+      );
+    }
+  }
+
+  fecharModal(){
+    this.form.reset();
+    this.displayModal = false;
   }
 
   iniciarForm(){
     this.form = this.fb.group({
-      nome:[],
-	    cpf: [],
-	    email: [],
-	    data: []
+      id: [null],
+      nome: [null, [Validators.required]],
+	    cpf: [null, [Validators.required, Validators.maxLength(11), Validators.minLength(11)]],
+	    email: [null, [Validators.required, Validators.email]],
+	    data: [null, [Validators.required]]
     })
   }
 
-  ngOnInit(): void {
-    this.buscarTodo();
-    this.iniciarForm();
+  excluir(id){
+    this.usuarioService.excluir(id).subscribe(
+      ()=>{
+        this.buscarTodo();
+        this.notification.addSuccessMessage("Usuario excluído");
+      },
+      ()=>{
+        this.buscarTodo();
+        this.notification.addErrorMessage("Erro ao excluir");
+      }
+    )
   }
-
 }
