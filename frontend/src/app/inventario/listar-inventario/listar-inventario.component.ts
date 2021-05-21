7e5121bbf9993d0801189ee2f7143f2850364acf
SavioAraujoPagung <savio.pagung@hotmail.com>
2021-05-21 20:31:40 -0300

    FRONTENDCRUDITENS - atualizacoes

--- frontend/src/app/inventario/listar-inventario/listar-inventario.component.ts
+++ frontend/src/app/inventario/listar-inventario/listar-inventario.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from './../../services/categoria.service';
+import { ItemService } from 'src/app/services/item.service';
+import { Item } from './../../shared/models/item.model';
+import { Categoria } from './../../shared/models/categoria.model';
+import { AlterarItensComponent } from 'src/app/inventario/alterar-itens/alterar-itens.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { SelectItem } from 'primeng';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listar-inventario',
@@ -7,9 +17,133 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarInventarioComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  @BlockUI() blockUI: NgBlockUI;
+  @ViewChild("dialogItem") dialogItem: AlterarItensComponent;
+  private _mensagemBlockUi: String = 'Carregando...';
+
+  categoria: Categoria;
+  categorias: Categoria[] = [];
+  itens: Item[];
+  form: FormGroup;
+  selectedItem: Item;
+  itemSelecionado: Item;
+  displayDialog: boolean;
+  sortOptions: SelectItem[];
+  sortKey: string;
+  sortField: string;
+  sortOrder: number;
+
+
+  constructor(
+    private itemService: ItemService,
+    private fb: FormBuilder,
+    private categoriaService: CategoriaService
+    ) { }
+
+  ngOnInit() {
+      this.iniciarForm();
+      this.buscarTodos();
+      this.buscarCategorias();
+      console.log("categorias" );
+      console.log(this.categorias);
+
+      this.sortOptions = [
+          {label: 'Nome A->Z', value: 'nome'},
+          {label: 'Nome Z->A', value: '!nome'},
+          {label: 'Categoria A->Z', value: 'categoriaId'},
+          {label: 'Categoria Z->A', value: '!categoriaId'}
+      ];
   }
 
+  buscarTodos(){
+    this.blockUI.start(this._mensagemBlockUi);
+    this.itemService.listar().pipe(
+      finalize(()=>{
+        this.blockUI.stop();
+      })
+    ).subscribe(
+      (itens) => {
+        this.itens = itens;
+        this.itens = this.montarImagem(this.itens);
+      }
+    )
+  }
+
+  buscarCategorias(){
+    this.categoriaService.buscarTodos().pipe(
+      finalize(()=>{
+        this.blockUI.stop();
+      })
+    ).subscribe(
+      (categorias) => {
+        this.categorias = categorias;
+        console.log(categorias+ "buscar categorias");
+      }
+    )
+  }
+
+  iniciarForm(){
+    this.form = this.fb.group({
+      id: [null],
+      nome: [null, [Validators.required]],
+	    imagem: [null, [Validators.required]],
+	    descricao: [null, [Validators.required]],
+	    disponibilidade: [null, [Validators.required]],
+	    usuarioId: [null, [Validators.required]],
+	    categoriaId: [null, [Validators.required]]
+    })
+  }
+
+  onSortChange(event) {
+    let value = event.value;
+
+    if (value.indexOf('!') === 0) {
+        this.sortOrder = -1;
+        this.sortField = value.substring(1, value.length);
+    }
+    else {
+        this.sortOrder = 1;
+        this.sortField = value;
+    }
+  }
+
+
+  selectItem(event: Event, item: Item) {
+    this.categorias.forEach(cat =>{
+
+      if(cat.id == item.categoriaId){
+      }
+    })
+    console.log("NOME DA CATEGORIAAAAAAA: " + this.categorias);
+    this.selectedItem = item;
+    this.displayDialog = true;
+    event.preventDefault();
+  }
+   alterarItem(event: Event, item: Item) {
+     this.itemSelecionado = item;
+     console.log(this.itemSelecionado);
+      this.dialogItem.abrir(this.itemSelecionado);
+
+     event.preventDefault();
+   }
+
+  onDialogHide() {
+    this.selectedItem = null;
+  }
+
+  
+  montarImagem(itens: Item[]){
+    itens.forEach(element => {
+      let formatoImagem = "data:image/jpg;base64,";
+      let imagem = formatoImagem.concat(element.imagem);
+      element.imagem = imagem;
+    });
+    return itens;
+  }
+
+  alterar(){
+    console.log(this.selectedItem);
+    this.dialogItem.abrir(this.selectedItem);
+  }
 }
